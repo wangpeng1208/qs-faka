9cd3791013d538dff28570edfb4995016c28c58e
王鹏 <286204069@qq.com>
2024-04-28 11:33:42 +0800

    收款通道啊添加验证器

--- app/adminapi/validate/channel/CollectionValidate.php
+++ app/adminapi/validate/channel/CollectionValidate.php
@@ -16,26 +16,26 @@ use taoser\Validate;
 
 class CollectionValidate extends Validate
 {
-    
+
     protected $rule = [
-        'title'    => 'require',
-        'code'  => 'require',
-        'mobile_code' => 'require',
-        'checkboxEd'  => 'require',
-        'invite_code' => 'require',
+        'title'          => 'require',
+        'code'           => 'require',
+        'show_name'      => 'require',
+        'account_fields' => 'require',
     ];
     protected $message = [
-        'title.require'    => '通道名称不能为空',
-        'code.unique'     => '代码接口不能为空',
-
+        'title.require'          => '通道名称不能为空',
+        'code.unique'            => '代码接口不能为空',
+        'show_name.require'      => '显示名称不能为空',
+        'account_fields.require' => '账号字段不能为空',
     ];
     public function sceneCollection()
     {
-        return $this->only(['title', 'code']);
+        return $this->only(['title', 'code', 'show_name', 'account_fields']);
     }
 
     public function sceneSettlement()
     {
-        return $this->only(['title', 'code']);
+        return $this->only(['title', 'code', 'account_fields']);
     }
 }
