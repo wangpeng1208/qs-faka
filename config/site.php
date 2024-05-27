cbf6983bdad2c40bbc20dd3e1a9737ae7459b60e
王鹏 <286204069@qq.com>
2024-05-27 20:13:40 +0800

    版本号更新

--- config/site.php
+++ config/site.php
@@ -9,7 +9,7 @@ return [
         'original' => '原生风格',
     ],
     // 当前版本号
-    'version'          => '1.2.15',
+    'version'          => '1.2.21',
     // 是否是演示模式
     'is_demo'          => false,
     // 远程api地址
@@ -22,11 +22,11 @@ return [
     'api_license'     => [
         [
             'name' => '使用协议',
-            'url'  => 'https://www.qqss.net/license.html',
+            'url'  => 'https://gitee.com/wp1208/faka-qs/wikis/%E4%BD%BF%E7%94%A8%E5%8D%8F%E8%AE%AE',
         ],
         [
             'name' => '教程手册',
-            'url'  => 'https://www.qqss.net/doc.html',
+            'url'  => 'https://gitee.com/wp1208/faka-qs/wikis/%E7%B3%BB%E7%BB%9F%E4%BB%8B%E7%BB%8D',
         ],
         [
             'name' => '官网链接',
