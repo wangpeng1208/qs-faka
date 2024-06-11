c8f398958b84caff19f49cd396c1bf79b107ecc5
王鹏 <286204069@qq.com>
2024-06-12 02:09:02 +0800

    增加一个 redis队列插件 使用示例

--- app/adminapi/controller/merchant/User.php
+++ app/adminapi/controller/merchant/User.php
@@ -18,6 +18,7 @@ use app\service\message\EmailMessageService;
 use app\common\model\{User as UserModel, UserCollect as UserCollectModel, Channel as ChannelModel, UserLoginLog, UserRate as UserRateModel, UserRoleRelation, UserLoginErrorLog};
 use app\service\message\MessageService;
 use Webman\Event\Event;
+use Webman\RedisQueue\Redis;
 
 /**
  * 商户管理
@@ -132,7 +133,13 @@ class User extends Base
         $id = inputs('id/d', 0);
         UserModel::destroy($id);
         // 删除分组
-        UserRoleRelation::where('user_id', $id)->delete();
+        $queue = 'del-merchant';
+        // 数据，可以直接传数组，无需序列化
+        $data = ['user_id' => $id];
+        // 投递消息
+        Redis::send($queue, $data);
+        // 投递延迟消息，消息会在60秒后处理
+        Redis::send($queue, $data, 60);
         $this->success('操作成功');
     }
 
---
+++ app/queue/redis/DelMerchant.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\queue\redis;
+
+use Webman\RedisQueue\Consumer;
+use app\common\model\UserRoleRelation;
+
+class DelMerchant implements Consumer
+{
+    // 要消费的队列名
+    public $queue = 'del-merchant';
+
+    // 连接名，对应 plugin/webman/redis-queue/redis.php 里的连接`
+    public $connection = 'default';
+
+    // 消费
+    public function consume($data)
+    {
+        // 无需反序列化
+        // var_export($data);
+        $user_id = $data['user_id'];
+        UserRoleRelation::where('user_id', $user_id)->delete();
+        // 继续用来扩展更多 比如 删除商户下的商品等等
+        
+    }
+            
+}
