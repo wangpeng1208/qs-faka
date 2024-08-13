f3cf0a0f541955032945589ff482690e3758645f
王鹏 <286204069@qq.com>
2024-08-13 19:40:55 +0800

    Deleted Invalid comment

--- app/command/RebuildTableCommand.php
+++ app/command/RebuildTableCommand.php
@@ -12,7 +12,6 @@
 
 namespace app\command;
 
-use app\common\model\User;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -30,16 +29,11 @@ class RebuildTableCommand extends Command
         // 清空数据库表
         $rebuild_table_name = [ 'auto_unfreeze', 'cash', 'order_complaint', 'order_complaint_message', 'gateway_goods', 'gateway_order', 'goods', 'goods_card', 'goods_category', 'goods_coupon', 'invite_code', 'article_read', 'link',  'merchant_log', 'merchant_message', '`order`', 'order_api', 'order_card', 'order_master', 'shop_list', 'shop_verify',    'user', 'user_analysis', 'user_apply', 'user_channel', 'user_collect', 'user_login_error_log', 'user_login_log', 'user_money_log', 'user_proxy', 'user_rate', 'user_risk',  'shop_iplist',  'user_role_relation', 'admin_log','channel_account','channel_custompay_order','chat_message','wechat_fans','article'];
 
-        // 用户资金 清空用户资金相关
-        // $rebuild_table_name = ['`order`', 'user_analysis', 'user_money_log', 'auto_unfreeze', 'order_master', 'admin_log', 'merchant_log', 'user_login_log', 'order_complaint', 'order_complaint_message', 'order_card'];
+
         foreach ($rebuild_table_name as $table_name) {
             $output->writeln("正在清空表：{$table_name}...");
             Db::query('truncate table ' . $table_name);
         }
-
-        // 用户资金测试
-        // User::where(['id' => 1])->update(['money' => 0, 'freeze_money' => 0, 'fee_money' => 200]);
-        // User::where(['id' => 2])->update(['money' => 0, 'freeze_money' => 0, 'fee_money' => 200]);
 
         // $this->execute($input,  $output);
         $timeUsed = time() - $triggerTime;
