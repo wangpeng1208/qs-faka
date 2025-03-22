96e7fe0d67092f2d12860f93dc326622c897c528
契约 <286204069@qq.com>
2025-03-23 07:20:43 +0800

    资金日志操作类型字典优化

--- server/app/common/model/UserMoneyLog.php
+++ server/app/common/model/UserMoneyLog.php
@@ -16,6 +16,25 @@ class UserMoneyLog extends BaseModel
 {
     protected $autoWriteTimestamp = true;
     protected $createTime = 'create_at';
+
+    // 模型操作字典 资金操作类型
+    public static $businessTypeDict = [
+        'unfreeze'                => '解冻金额',
+        'freeze'                  => '冻结金额',
+        'cash_notpass'            => '提现未通过',
+        'cash_success'            => '提现成功',
+        'apply_cash'              => '申请提现',
+        'admin_inc'               => '后台操作加钱',
+        'admin_dec'               => '后台操作扣钱',
+        'fee'                     => '手续费',
+        'goods_sold'              => '卖出商品',
+        'goods_refund'            => '商品退款',
+        'sub_register'            => '推广注册奖励',
+        'reward'                  => '奖励金',
+        'admin_deposit_money_inc' => '管理员手动操作增加押金',
+        'admin_deposit_money_dec' => '管理员手动操作扣除押金',
+    ];
+    
     public function user()
     {
         return $this->belongsTo('User', 'user_id');
@@ -40,7 +59,7 @@ class UserMoneyLog extends BaseModel
 
     public function searchDateRangeAttr($query, $value, $data)
     {
-        if (empty ($value) || empty (trim($value[0])) || empty (trim($value[1]))) {
+        if (empty($value) || empty(trim($value[0])) || empty(trim($value[1]))) {
             return;
         } else {
             $query->whereBetweenTime('create_at', $value[0], $value[1]);
