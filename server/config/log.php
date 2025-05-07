dadaa32d93d5b039649e7dde230bf30ff8dd9a6d
契约 <286204069@qq.com>
2025-05-08 04:21:25 +0800

    增加支付错误记录日志通道

--- server/config/log.php
+++ server/config/log.php
@@ -77,4 +77,20 @@ return [
             ]
         ],
     ],
+    'pay' => [
+        'handlers' => [
+            [
+                'class' => Monolog\Handler\RotatingFileHandler::class,
+                'constructor' => [
+                    runtime_path() . '/logs/pay-error.log',
+                    7, // $maxFiles
+                    Monolog\Logger::DEBUG,
+                ],
+                'formatter' => [
+                    'class' => Monolog\Formatter\LineFormatter::class,
+                    'constructor' => [null, 'Y-m-d H:i:s', true],
+                ],
+            ]
+        ],
+    ],
 ];
