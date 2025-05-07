ff2395b8a1d84383672173cff5a1da7a7d065b27
契约 <286204069@qq.com>
2025-05-08 04:28:13 +0800

    文档地址链接更新

--- server/config/site.php
+++ server/config/site.php
@@ -13,12 +13,12 @@ return [
     // 版权信息
     'api_license'     => [
         [
-            'name' => '使用协议',
-            'url'  => 'https://gitee.com/wp1208/faka-qs/wikis/pages?sort_id=10911613&doc_id=5441578',
+            'name' => '使用协议（MTT）',
+            'url'  => '#',
         ],
         [
             'name' => '开发手册',
-            'url'  => 'https://gitee.com/wp1208/faka-qs/wikis/pages',
+            'url'  => 'https://www.qqss.net/manual/qs',
         ],
         [
             'name' => '官网链接',
