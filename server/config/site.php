a49e12b7845c5c92d3f837af80f0877a6ff32cf0
契约 <286204069@qq.com>
2025-06-14 10:14:11 +0800

    1.5.7版本号

--- server/config/site.php
+++ server/config/site.php
@@ -9,7 +9,7 @@ return [
         'original' => '原生风格',
     ],
     // 当前版本号
-    'version'          => '1.5.6',
+    'version'          => '1.5.7',
     // 版权信息
     'api_license'     => [
         [
